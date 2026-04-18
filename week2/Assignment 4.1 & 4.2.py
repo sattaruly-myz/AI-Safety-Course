a167cc65b3ea1d6412ac07d17e9f90364da19944
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-04-18 18:18:18 +0500

    [week2]-update_Assignment 4.1 & 4.2.py

--- week2/Assignment 4.1 & 4.2.py
+++ week2/Assignment 4.1 & 4.2.py
@@ -6,3 +6,13 @@ for k in k_values:
     accuracies.append(m)
     ci_lowers.append(l)
     ci_uppers.append(u)
+
+# это 4.2
+question_ids = list(range(100))
+np.random.seed(42)
+for n in dataset_sizes:
+    sim_scores = np.random.binomial(1, 0.6, n)
+    l, m, u = ci_accuracy_basic(sim_scores)
+    accuracies.append(m)
+    ci_lowers.append(l)
+    ci_uppers.append(u)
