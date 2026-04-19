16d71ef2d7bedc4e6a1c881f537aea74fc45a90d
Myrzabek <myrzabeksatybaldinov23@gmail.com>
2026-04-19 10:37:06 +0500

    [week1]-add_Assignment 1.py

---
+++ week1/Assignment 1.py
@@ -0,0 +1,40 @@
+from inspect_ai import Task, task, eval
+from inspect_ai.dataset import Sample
+from inspect_ai.scorer import match
+from inspect_ai.solver import generate
+
+@task
+def hello_model():
+    return Task(
+        dataset=[
+            Sample(
+                input="Say 'Hello world!' and nothing else.",
+                target="Hello world!"
+            ),
+            Sample(
+                input="2+2=",
+                target="4"
+            ),
+            Sample(
+                input="What is the surname of Sheldon from The Big Bang Theory?",
+                target="Cooper"
+            ),
+            Sample(
+                input="What is the capital of Kazakhstan?",
+                target="Astana"
+            )
+        ],
+        solver=[generate()],
+        scorer=match(
+            location="end",
+            ignore_case=True,
+            numeric=False
+        )
+    )
+
+if __name__ == "__main__":
+    eval(
+        hello_model,
+        model="ollama/qwen2.5:7b", 
+        # limit=1 # Раскомментируй для теста только на 1 примере
+    )
